9a689aa850a066e68cf955bce848319222f6c09e
omshetye <omega10303@gmail.com>
2025-09-21 19:33:59 +0530

    created build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# build.sh
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Any other setup commands
+python manage.py collectstatic --noinput
+python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi
--- ecomm/settings.py
+++ ecomm/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -140,10 +141,7 @@ WSGI_APPLICATION = 'ecomm.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default="postgres://username:password@host:port/dbname")
 }
 
 
