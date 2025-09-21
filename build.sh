2135386a1ecc6a5c7be36dae662ef166ed70be3d
omshetye <omega10303@gmail.com>
2025-09-21 20:07:39 +0530

    settings.py and crlf for build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-# build.sh
+set -e
 
 # Install dependencies
 pip install -r requirements.txt
 
-# Any other setup commands
+# Collect static files
 python manage.py collectstatic --noinput
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+
+# Create superuser if CREATE_SUPERUSER is set
+if [[ "$CREATE_SUPERUSER" == "1" ]]; then
+  echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() or \
+User.objects.create_superuser(username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD')" \
+  | python manage.py shell
 fi
