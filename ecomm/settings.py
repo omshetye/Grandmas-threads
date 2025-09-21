58b503e96fe76779ffa57d78abed784d8ed3b550
omshetye <omega10303@gmail.com>
2025-09-21 21:39:13 +0530

    changed settings

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -197,11 +197,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "public/media")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "public/media")]
 
 # Media files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
+# MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
