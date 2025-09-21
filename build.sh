509243a3df8e6ec1c226e1894a82537139d381cd
omshetye <omega10303@gmail.com>
2025-09-21 20:27:42 +0530

    build.sh edited

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
-# Install dependencies
+echo "=== Installing dependencies ==="
 pip install -r requirements.txt
 
-# Collect static files
+echo "=== Running migrations ==="
+python manage.py migrate
+
+echo "=== Collecting static files ==="
 python manage.py collectstatic --noinput
 
-# Create superuser if CREATE_SUPERUSER is set
+echo "=== Creating superuser if needed ==="
 if [[ "$CREATE_SUPERUSER" == "1" ]]; then
   echo "from django.contrib.auth import get_user_model; \
 User = get_user_model(); \
@@ -15,3 +18,5 @@ User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() or \
 User.objects.create_superuser(username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD')" \
   | python manage.py shell
 fi
+
+echo "=== Build completed ==="
